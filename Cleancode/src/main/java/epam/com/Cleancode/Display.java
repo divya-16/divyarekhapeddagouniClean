57e72a1adf4284220b9c79622e86538ae5117d5e
divya-16 <18wh5a0515@bvrithyderabad.edu.in>
2020-03-03 11:08:26 +0530

    commit

---
+++ Cleancode/src/main/java/epam/com/Cleancode/Display.java
@@ -0,0 +1,10 @@
+package epam.com.Cleancode;
+
+public class Display extends SimpleandCompund {
+	public static void main(String[] args){
+		Display invoke = new Display();
+		invoke.inputDetails();
+		
+	}
+
+}
---
+++ Cleancode/src/main/java/epam/com/Cleancode/Structure.java
@@ -0,0 +1,8 @@
+package epam.com.Cleancode;
+
+public class Structure {
+   public static void main(String[] args){
+	   House hs = new House();
+	   hs.dimensions();
+   }
+}
